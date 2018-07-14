ebe1db65c723c280be179309ee0579be49bb5106
DAEMonRaco <daemonraco@gmail.com>
2018-07-13 21:58:15 -0300

    DRTools
    * Using version 0.4.0.

--- server.js
+++ server.js
@@ -8,9 +8,20 @@ const bodyParser = require('body-parser');
 const chalk = require('chalk');
 const cors = require('cors');
 const express = require('express');
-const { ExpressConnector } = require('drtools');
 const http = require('http');
 const path = require('path');
+//
+// Importing DRTools.
+const {
+    ConfigsManager,
+    EndpointsManager,
+    ExpressConnector,
+    LoadersManager,
+    MiddlewaresManager,
+    RoutesManager,
+    PluginsManager,
+    TasksManager
+} = require('drtools');
 //
 // Creating an express application.
 const app = express();
@@ -29,22 +40,50 @@ loadingSteps.push(async () => {
     app.use(cors());
 });
 //
-// Loading DRTools and connecting them to ExpressJS.
+// Loading DRTools configs.
+loadingSteps.push(async () => {
+    global.configs = new ConfigsManager(path.join(__dirname, 'configs'), { publishConfigs: true });
+});
+//
+// Loading DRTools ExpressJS connector.
 loadingSteps.push(async () => {
     ExpressConnector.attach(app, {
-        configsDirectory: path.join(__dirname, 'configs'),
-        loadersDirectory: path.join(__dirname, 'includes/loaders'),
-        endpoints: {
-            directory: path.join(__dirname, 'includes/mock-endpoints'),
-            uri: 'mock-api/v1.0'
-        },
-        middlewaresDirectory: path.join(__dirname, 'includes/middlewares'),
-        routesDirectory: path.join(__dirname, 'includes/routes'),
-        tasksDirectory: path.join(__dirname, 'includes/tasks'),
-        pluginsDirectories: path.join(__dirname, 'plugins'),
-        webUi: true
+        webUi: global.configs.get('environment').drtools.webUi
     });
-    global.drtools = ExpressConnector.attachments();
+});
+//
+// Loading DRTools loaders.
+loadingSteps.push(async () => {
+    new LoadersManager(path.join(__dirname, 'includes/loaders'), {}, global.configs);
+});
+//
+// Loading DRTools middlewares.
+loadingSteps.push(async () => {
+    new MiddlewaresManager(app, path.join(__dirname, 'includes/middlewares'), {}, global.configs);
+});
+//
+// Loading DRTools plugins.
+loadingSteps.push(async () => {
+    new PluginsManager(path.join(__dirname, 'plugins'), {}, global.configs);
+});
+//
+// Loading DRTools routes.
+loadingSteps.push(async () => {
+    new RoutesManager(app, path.join(__dirname, 'includes/routes'), {}, global.configs);
+});
+//
+// Loading DRTools tasks.
+loadingSteps.push(async () => {
+    new TasksManager(path.join(__dirname, 'includes/tasks'), {}, global.configs);
+});
+//
+// Loading DRTools mock-endpoints.
+loadingSteps.push(async () => {
+    const endpoints = new EndpointsManager({
+        directory: path.join(__dirname, 'includes/mock-endpoints'),
+        uri: 'mock-api/v1.0'
+    }, global.configs);
+    app.use(endpoints.provide());
 });
 //
 // Setting static folders.
